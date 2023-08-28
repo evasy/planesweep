d6ea03203e97a2a8614ec92aba34cd4615711127
evasy <yitongshan2016@yahoo.com>
2023-08-28 08:48:34 -0700

    code

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.sound.sampled.Line;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -6,16 +8,9 @@ import java.util.TreeMap;
 
 public class Main {
 
-    static double[] test1 = Data.test1;
-    static double[] test2 = Data.test2;
-
-    /**
-     * return the squared distance between two coordinates
-     * using latitude and longitude
-     */
-    public static double distanceLatLngSq(double lat1, double lat2, double lon1, double lon2) {
-        return Math.pow(((lat1-lat2)*111139),2) + Math.pow(((lon1 - lon2)*111139),2);
-    }
+    static ArrayList<Coordinate> test1 = convertToCoordinates(Data.test1);
+    static ArrayList<Coordinate> test2 = convertToCoordinates(Data.test2);
+    static double[] mainline;
 
     /**
      * convert the numbers from the data list into coordinate object
@@ -30,52 +25,133 @@ public class Main {
         return l;
     }
 
+    /**
+     * convert the Coordinates from the data list into line segment object
+     * return an arraylist of LineSegment
+     */
+    public static ArrayList<LineSegment> convertToLineSegments(ArrayList<Coordinate> list) {
+        ArrayList<LineSegment> lineSegments = new ArrayList<>();
+        for (int i = 0; i < list.size() - 2; i += 1) {
+            LineSegment segment = new LineSegment(list.get(i), list.get(i + 1));
+            lineSegments.add(segment);
+        }
+        return lineSegments;
+    }
 
     /**
-     * find the main line equation y=ax+b and return a and b from
-     * return an Array that the first element is a and second element is b
+     * return the squared distance between two coordinates
+     * using latitude and longitude
      */
-    public double[] mainLine(Coordinate c1, Coordinate c2) {
-
+    public static double distanceLatLngSq(Coordinate c1, Coordinate c2) {
+        double lat1 = c1.getLatitude();
+        double lat2 = c2.getLatitude();
+        double lon1 = c1.getLongitude();
+        double lon2 = c2.getLongitude();
+        return Math.pow(((lat1-lat2)*111139),2) + Math.pow(((lon1 - lon2)*111139),2);
     }
 
 
     /**
-     * check if a equals to zero, and find the equation of the end point from test A
+     * find the main line equation y=ax+b
+     * return an Array that the first element is slope 'a'
+     * and second element is y-intercept 'b'.
+     */
+    public static double[] findLine(Coordinate c1, Coordinate c2) {
+        double[] res = new double[2];
+        res[0] = (c1.getLongitude() - c2.getLongitude())/(c1.getLatitude() - c2.getLatitude());
+        res[1] = c1.getLongitude() - res[0] * c1.getLatitude();
+        return res;
+    }
+
+    /**
+     * find the intersection of two lines
+     * return the Coordinate of the intersection
+     */
+    public static Coordinate findIntersection(double[] line1, double[] line2) {
+        double lat = (line2[1] - line1[1])/(line1[0] - line2[0]);
+        double lon = line1[0] * lat + line1[1];
+        Coordinate intersection = new Coordinate(lat, lon);
+        return intersection;
+    }
+
+
+    /**
+     * find the equation of the end point from test A
      * and its perpendicular line to the main line
      * return the line segment of end point and intersection on the perpendicular line
      */
-    public double[] perpendicularLine(Coordinate A, double[] mainline) {
-
-    }
-
-    /**
-     * find the line segment which has an intersection with the perpendicular line
-     * return the line segment object
-     */
-    public LineSegment intersectLineSegment() {
-
+    public static double[] perpendicularLine(Coordinate A) {
+        double[] res = new double[2];
+        res[0] = -1/mainline[0];
+        res[1] = A.getLongitude() + A.getLatitude()/mainline[0];
+        return res;
     }
 
 
     /**
-     * find the intersection point
-     * return the Coordinate
+     * find the line segment from testB that has an intersection with the perpendicular line
+     * and find the intersection,
+     * return the coordinate of the intersection
      */
-    public Coordinate[] findIntersection(LineSegment l1, LineSegment l2) {
+    public static Coordinate intersectionLineSegment(Coordinate A, ArrayList<LineSegment> testB) {
+        double x;
+        LineSegment lineb = null;
+        Coordinate res = null;
+        // check if slope equals to zero
+        if (mainline[0] == 0) {
+            x = A.getLatitude();
+            for (LineSegment l : testB) {
+                if (l.start.getLatitude() <= x && x <= l.end.getLatitude()) {
+                    lineb = l;
+                }
+            }
+            double[] b = findLine(lineb.end, lineb.start);
+            res = new Coordinate(x, x*b[0] + b[1]);
+        }
+        else {
+            double[] perpendicular = perpendicularLine(A);
+            for (LineSegment l : testB) {
+                double bound1 = perpendicular[0] * l.start.getLatitude() + perpendicular[1];
+                double bound2 = perpendicular[0] * l.end.getLatitude() + perpendicular[1];
+                double segement_y_start = l.start.getLongitude();
+                double segement_y_end = l.end.getLongitude();
+                if (Math.max(bound1, bound2) >= Math.max(segement_y_start, segement_y_end) &&
+                        Math.min(bound1, bound2) <= Math.min(segement_y_start, segement_y_end)) {
+                    lineb = l;
+                }
+            }
+            if(lineb == null) {
+                return null;
+            }
+            double[] b = findLine(lineb.end, lineb.start);
+            res = findIntersection(b, perpendicular);
+        }
 
+        return res;
     }
 
     /**
-     * return the max distance of all the distances from test A endpoints to test B line segments
+     * return the max distance of all the distances from A endpoints to test B line segments
      */
-    public static double maxDistance(double[] testA, double[] testB) {
-        double maxdist;
+    public static double maxDistance(ArrayList<Coordinate> coordinatesA, ArrayList<Coordinate> testB) {
+        double maxdist = 0;
+        ArrayList<LineSegment> linesegmentsB = convertToLineSegments(testB);
+        for (Coordinate a : coordinatesA) {
+            Coordinate b = intersectionLineSegment(a, linesegmentsB);
+            if (b != null) {
+                double temp = distanceLatLngSq(a,b);
+                if (temp > maxdist) {
+                    maxdist = temp;
+                }
+            }
+        }
         return Math.sqrt(maxdist);
     }
 
     public static void main(String[] args) {
-        return maxDistance(test1, test2);
+        mainline = findLine(test2.get(0), test1.get(test2.size()-1));
+        System.out.println(maxDistance(test1, test2));
+        System.out.println(maxDistance(test2, test1));
     }
 
 
